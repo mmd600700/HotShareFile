0805cf6b10c2d6efbe4dda0d133f7593cc723f03
cnuf <167686703+cnuf791@users.noreply.github.com>
2026-08-06 15:27:46 +0330

    MainActivity

--- app/src/main/java/com/hotsharefile/hotsharefile/MainActivity.kt
+++ app/src/main/java/com/hotsharefile/hotsharefile/MainActivity.kt
@@ -227,12 +227,8 @@ fun HotShareApp(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFF121212))
-             .padding(
-                top = 20.dp,  
-                bottom = 45.dp, 
-                start = 16.dp, 
-                end = 16.dp
-            ),
+            .navigationBarsPadding()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box(
